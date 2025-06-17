08930f00963bbdd4fc4b01e2b6f8d20a53a5d132
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-06-17 12:43:56 -0500

    add constructors

--- src/main/java/com/notevault/models/Role.java
+++ src/main/java/com/notevault/models/Role.java
@@ -3,20 +3,14 @@ package com.notevault.models;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.notevault.enums.AppRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Table(name = "roles")
-public class Role{
+public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,5 +29,75 @@ public class Role{
 
     public Role(AppRole roleName) {
         this.roleName = roleName;
+    }
+
+    public Role(Integer roleId, AppRole roleName, Set<User> users) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+        this.users = users;
+    }
+
+    public Role() {
+    }
+
+    public Integer getRoleId() {
+        return this.roleId;
+    }
+
+    public AppRole getRoleName() {
+        return this.roleName;
+    }
+
+    public Set<User> getUsers() {
+        return this.users;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public void setRoleName(AppRole roleName) {
+        this.roleName = roleName;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Role)) return false;
+        final Role other = (Role) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$roleId = this.getRoleId();
+        final Object other$roleId = other.getRoleId();
+        if (this$roleId == null ? other$roleId != null : !this$roleId.equals(other$roleId)) return false;
+        final Object this$roleName = this.getRoleName();
+        final Object other$roleName = other.getRoleName();
+        if (this$roleName == null ? other$roleName != null : !this$roleName.equals(other$roleName)) return false;
+        final Object this$users = this.getUsers();
+        final Object other$users = other.getUsers();
+        if (this$users == null ? other$users != null : !this$users.equals(other$users)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Role;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $roleId = this.getRoleId();
+        result = result * PRIME + ($roleId == null ? 43 : $roleId.hashCode());
+        final Object $roleName = this.getRoleName();
+        result = result * PRIME + ($roleName == null ? 43 : $roleName.hashCode());
+        final Object $users = this.getUsers();
+        result = result * PRIME + ($users == null ? 43 : $users.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Role(roleId=" + this.getRoleId() + ")";
     }
 }
