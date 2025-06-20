cebc4ff61217357d587cd3933055b3a84637fc1c
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-06-20 03:56:52 -0500

    add PreAuthorize annotation

--- src/main/java/com/notevault/controllers/AdminController.java
+++ src/main/java/com/notevault/controllers/AdminController.java
@@ -6,25 +6,28 @@ import com.notevault.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+//@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     @Autowired
     private UserService userService;
 
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getusers")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @PutMapping("update-role")
-    public ResponseEntity<String> updateUserRole(@RequestParam Long userId, @RequestParam String role) {
-        userService.updateUserRole(userId,role);
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId, @RequestParam String roleName) {
+        userService.updateUserRole(userId,roleName);
         return new ResponseEntity<>("Role updated successfully", HttpStatus.OK);
     }
 
