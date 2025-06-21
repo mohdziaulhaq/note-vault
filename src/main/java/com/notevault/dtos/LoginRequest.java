766e1b2080efe0b3b95c8ca3f4bb1004433b4752
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-06-21 12:02:22 -0500

    add LoginRequest.java dto to take login request

---
+++ src/main/java/com/notevault/dtos/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.notevault.dtos;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
