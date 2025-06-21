8918d1507427e368035c8ed5282ef33174c969ba
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-06-21 06:29:22 -0500

    add logger and remove unused imports

--- src/main/java/com/notevault/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/notevault/security/jwt/AuthTokenFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,18 +25,18 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Autowired
     private UserDetailsServiceImpl userDetailsServiceImpl;
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         logger.debug("AuthTokenFilter called for URI: " + request.getRequestURI());
         try {
-
-
             String jwt = parseJwt(request);
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
                 String username = jwtUtils.getUsernNameFromJwtToken(jwt);
                 UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                logger.debug("Roles from JWT : {}", userDetails.getAuthorities());
+                logger.debug("Roles from JWT :"+ userDetails.getAuthorities());
 
                 authentication.setDetails(userDetails);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
