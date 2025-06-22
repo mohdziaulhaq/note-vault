51fa60d127b73bd0a77c5684a9e91ac073d505d7
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-06-22 02:22:00 -0500

    add existsbyemail endpoint

--- src/main/java/com/notevault/repositories/UserRepository.java
+++ src/main/java/com/notevault/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.notevault.repositories;
 
 import com.notevault.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
@@ -10,5 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String username);
 
     Boolean existsByUserName(String username);
+
+    Boolean existsByEmail(String email);
 }
 
