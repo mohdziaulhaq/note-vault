a11bd686e07fc3436faea9a21ca263fabe8fff8d
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-16 02:04:45 -0500

    add AuditLogServiceImpl.java

---
+++ src/main/java/com/notevault/services/impl/AuditLogServiceImpl.java
@@ -0,0 +1,55 @@
+package com.notevault.services.impl;
+
+import com.notevault.models.AuditLog;
+import com.notevault.models.Note;
+import com.notevault.repositories.AuditLogRepository;
+import com.notevault.services.AuditLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AuditLogServiceImpl implements AuditLogService {
+
+    @Autowired
+    private AuditLogRepository auditLogRepository;
+
+    @Override
+    public void logNoteCreation(String username, Note note) {
+        AuditLog log = new AuditLog();
+        log.setAction("CREATE");
+        log.setUsername(username);
+        log.setNoteId(note.getId());
+        log.setNoteContent(note.getContent());
+        log.setTimeStamp(LocalDateTime.now());
+
+        auditLogRepository.save(log);
+    }
+
+
+    @Override
+    public void logNoteUpdate(String username, Note note) {
+        AuditLog log = new AuditLog();
+        log.setAction("UPDATE");
+        log.setUsername(username);
+        log.setNoteId(note.getId());
+        log.setNoteContent(note.getContent());
+        log.setTimeStamp(LocalDateTime.now());
+
+        auditLogRepository.save(log);
+    }
+
+    @Override
+    public void logNoteDeletion(String username, Long noteId) {
+        AuditLog log = new AuditLog();
+        log.setAction("DELETE");
+        log.setUsername(username);
+        log.setNoteId(noteId);
+        log.setTimeStamp(LocalDateTime.now());
+
+        auditLogRepository.save(log);
+    }
+
+
+}
