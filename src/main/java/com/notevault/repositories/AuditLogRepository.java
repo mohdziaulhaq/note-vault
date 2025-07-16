9a7ebe14c212ce9469fa6f7977f3793e291b6d3e
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-16 02:13:58 -0500

    add findByNoteId method

--- src/main/java/com/notevault/repositories/AuditLogRepository.java
+++ src/main/java/com/notevault/repositories/AuditLogRepository.java
@@ -3,5 +3,8 @@ package com.notevault.repositories;
 import com.notevault.models.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByNoteId(Long noteId);
 }
