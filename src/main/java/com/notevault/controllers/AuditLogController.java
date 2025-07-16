853715fc03d8619fa998c1fd303e0e4168666508
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-16 04:31:47 -0500

    add method level security

--- src/main/java/com/notevault/controllers/AuditLogController.java
+++ src/main/java/com/notevault/controllers/AuditLogController.java
@@ -3,6 +3,7 @@ package com.notevault.controllers;
 import com.notevault.models.AuditLog;
 import com.notevault.services.AuditLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,13 @@ public class AuditLogController {
     private AuditLogService auditLogService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<AuditLog> getAllAuditLogs() {
-        auditLogService.getAllAuditLogs();
-        return null;
+       return auditLogService.getAllAuditLogs();
     }
 
     @GetMapping("/note/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<AuditLog> getAuditLogsByNoteId(@PathVariable Long id) {
         return auditLogService.getAuditLogsForNoteId(id);
     }
