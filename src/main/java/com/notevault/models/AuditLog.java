e659201977956a1348492147a292222b7f258e00
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-16 03:10:40 -0500

    add getter and setter methods

--- src/main/java/com/notevault/models/AuditLog.java
+++ src/main/java/com/notevault/models/AuditLog.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data
 public class AuditLog {
 
     @Id
@@ -20,4 +18,56 @@ public class AuditLog {
     private Long noteId;
     private String noteContent;
     private LocalDateTime timeStamp;
+
+    public AuditLog() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getAction() {
+        return this.action;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public Long getNoteId() {
+        return this.noteId;
+    }
+
+    public String getNoteContent() {
+        return this.noteContent;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return this.timeStamp;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setNoteId(Long noteId) {
+        this.noteId = noteId;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
 }
