98cf859e75baca35c750ebc18e15ecbb36518969
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-18 06:21:43 -0500

    add PasswordResetTokenRepository

---
+++ src/main/java/com/notevault/repositories/PasswordResetTokenRepository.java
@@ -0,0 +1,10 @@
+package com.notevault.repositories;
+
+import com.notevault.models.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository  extends JpaRepository<PasswordResetToken, Long> {
+
+}
